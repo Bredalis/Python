5b2fe5912e2bdc5ca17af09378b58a608f867566
PRO X <bredalisgautreaux@gmail.com>
2023-02-08 19:51:25 -0400

    Carpetas nuevas

---
+++ Programas/Books.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+
+Datos = {
+
+	"Desarrollo Personal": ["Ajedrez", "Piense y hagase rico", "Las 48 leyes del poder",
+	"Como ganar amigos e influir sobre las personas", "Filosofia", "Psicologia", "Habla menos actua mas", "Mente de crecimiento"],
+
+ 	"Programacion": ["SQL","Libro negro del programador", "Keras", "Pytorch", "Patrones de diseño", "Python", "Linux", "Mejor Programador"],
+
+ 	"Inteligencia Finaciera": ["Publicidad", "Probabilidad", "Inversiones", "Mercadotegnia", 
+ 	"Leyes de liderasgo", "Marketing Digital", "Estadistica", "El inversion Inteligente"],
+
+}
+
+Tabla_Libros = pd.DataFrame(Datos)
+
+print(Tabla_Libros)
---
+++ Programas/Plan_Aprendisaje.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+
+print("Pasos para aprender lo que sea")
+
+Plan = pd.Series([
+
+	"Plan",
+
+	"Se Autodidacta", "Ten Fundamentos", "Une Puntos", 
+	"Reta la mente", "Práctica", "Aplica", 
+	"Experimenta", "Ten Mentores"
+
+])
+
+print(Plan)
